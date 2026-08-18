6d67575a98054b21a225e9cf31655f6aa854a42b
Susman Gurung <susmangurung5@gmail.com>
2026-08-18 10:33:54 +0545

    feat: Adding a paragraph in a string"

--- test.py
+++ test.py
@@ -1,29 +1,37 @@
-print("hello, world!")
-# calculate product
-a = 5
-b = 3
-product = a * b 
-print(product) #will print the product of a and b
+# print("hello, world!")
+# # calculate product
+# a = 5
+# b = 3
+# product = a * b 
+# print(product) #will print the product of a and b
 
-# calculate sum
-print("sum",a+b)
+# # calculate sum
+# print("sum",a+b)
 
-# to calculate the sum of x and y (user input method)
-x=float(input("Enter the value of x:"))
-y=float(input("Enter the value of y:"))
-print("x+y: ",x+y)
+# # to calculate the sum of x and y (user input method)
+# x=float(input("Enter the value of x:"))
+# y=float(input("Enter the value of y:"))
+# print("x+y: ",x+y)
 
-#to calculate the difference between x and y (user input method)
-x=float(input("Enter the value of x:"))
-y+float(input("Enter the value of y:"))
-print("x-y: ",x-y)
+# #to calculate the difference between x and y (user input method)
+# x=float(input("Enter the value of x:"))
+# y+float(input("Enter the value of y:"))
+# print("x-y: ",x-y)
 
-# Print "Hello World!"
-# Print "Have a good day."
-# Print "Learning Python is fun!"
-print("hello world!")
-print("Have a good day!")
-print("Learning python is fun!")
+# # Print "Hello World!"
+# # Print "Have a good day."
+# # Print "Learning Python is fun!"
+# print("hello world!")
+# print("Have a good day!")
+# print("Learning python is fun!")
 
-# Print "I am" and the number 25
-print("I am",25)
+# # Print "I am" and the number 25
+# print("I am",25)
+
+#Creating a string in python 
+str1= "This is a string. We are creating in python."
+print(str1)
+
+#If want the sentence from "We" to be started on a new paragraph, use function "\n"
+str1="This is a string.\nWe are creating in python"
+print(str1)
